f2613682e92bd32c377d36486b35c6c829dc1795
Kirsten Werner <kirstenmwerner@gmail.com>
2019-04-01 19:15:59 +0000

    Automatically backed up by Learn

--- washington_ski_trip/lib/washington_ski_trip.rb
+++
@@ -1,6 +0,0 @@
-require "washington_ski_trip/version"
-
-module WashingtonSkiTrip
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- washington_ski_trip/lib/washington_ski_trip/version.rb
+++
@@ -1,3 +0,0 @@
-module WashingtonSkiTrip
-  VERSION = "0.1.0"
-end
